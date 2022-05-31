7efefa6a4a953a6052a40b6cf40a654135db8165
aidana <aidana.niiazova@gmail.com>
2022-05-31 15:38:50 +0600

    Updates

---
+++ precache-manifest.2696c73547cc92092da5992c35f2c0f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "850000e908f6b0bdb549",
+    "url": "/pomodoro-timer/static/css/main.3a10837d.chunk.css"
+  },
+  {
+    "revision": "850000e908f6b0bdb549",
+    "url": "/pomodoro-timer/static/js/main.850000e9.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/pomodoro-timer/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "87847e7ea075075527ee",
+    "url": "/pomodoro-timer/static/css/2.e8a63d6b.chunk.css"
+  },
+  {
+    "revision": "87847e7ea075075527ee",
+    "url": "/pomodoro-timer/static/js/2.87847e7e.chunk.js"
+  },
+  {
+    "revision": "b13413670e2f41b0c8a6",
+    "url": "/pomodoro-timer/static/js/runtime~main.b1341367.js"
+  },
+  {
+    "revision": "3cfe0ebbe384ce69e571b0a96e7a4da4",
+    "url": "/pomodoro-timer/index.html"
+  }
+];
--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-// import {declareAction} from "@reatom/core/experiments/v1";
-//
-// export const startAction = declareAction();
-// export const pauseAction = declareAction();
-// export const resetAction = declareAction();
-//
-// export const openSettingsAction = declareAction();
-// export const closeSettingsAction = declareAction();
-//
